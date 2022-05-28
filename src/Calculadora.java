cd1bcf3ab54de158cfad1868ecf29d787d4bf16c
Albierre <Albierrereis@gmail.com>
2022-05-27 21:30:58 -0300

    Adicionado C

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,11 +2,12 @@ public class Calculadora {
     public static void main(String[] args) {
         int A = 10;
         int B = 5;
+        int C = 15;
 
-        System.out.println("A multiplicação dos números é de: " + (A * B));
-        System.out.println("A divisão dos números é de: " + (A / B));
-        System.out.println("A soma dos números é de: " + (A + B));
-        System.out.println("A subtração dos números é de: " + (A - B));
+        System.out.println("A multiplicação dos números é de: " + (A * B * C));
+        System.out.println("A divisão dos números é de: " + (A / B / C));
+        System.out.println("A soma dos números é de: " + (A + B + C));
+        System.out.println("A subtração dos números é de: " + (A - B - C));
     }
 }
 
